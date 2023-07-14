73b12ee9292f25a6964eca01c69e2ac2d6a9c522
Bohumil Brož <127795959+brozb@users.noreply.github.com>
2023-07-14 14:30:55 +0200

    rename submodule

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="spot_wrapper_custom",
+    name="spot_wrapper",
     version="1.0.0",
     description="Wrapper for Boston Dynamics Spot SDK",
-    packages=["spot_wrapper_custom"],
+    packages=["spot_wrapper"],
     install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core"],
 )
